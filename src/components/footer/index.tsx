68d3170b3332afb8e13354e1993cc8c53f6e9b64
Ilya Tishin <nenaviju4tverg@gmail.com>
2025-02-24 15:21:01 -0800

    feat: update footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,21 +4,21 @@ import Logo from '@/components/logo/logo.tsx'
 
 const Footer: FC = () => (
   <footer className="bg-background">
-    <div className="mx-auto w-screen p-4 lg:px-6">
+    <div className="mx-auto w-screen p-2 lg:p-4">
       <div className="flex items-center justify-between">
         <div className="flex">
           <a href="/">
             <span className="flex items-center">
               <Logo className="fill-foreground h-8 w-auto" />
-              <span className="text-foreground ml-1 text-xl font-medium uppercase">
+              <span className="text-foreground mr-2 ml-1.5 text-sm font-medium uppercase sm:text-base">
                 Roseville Gutter
               </span>
             </span>
           </a>
         </div>
       </div>
-      <div className="border-foreground/10 mt-4 border-t pt-4 md:flex md:items-center md:justify-between">
-        <p className="text-foreground mt-8 text-sm/6 md:order-1 md:mt-0">
+      <div className="border-foreground mt-2 border-t pt-2 md:flex md:items-center md:justify-between">
+        <p className="text-foreground text-sm sm:mt-8 md:order-1 md:mt-0">
           &copy; {new Date().getFullYear()} Roseville Gutter. All rights
           reserved.
         </p>
